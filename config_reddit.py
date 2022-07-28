12d9a8dcccf4ab665c95d1f6ecfe09166a816fc1
Pauliduran <pduran@student.hult.edu>
2022-07-28 15:40:31 -0400

    Collect data
    Collect data

---
+++ config_reddit.py
@@ -0,0 +1,9 @@
+## Reddit API credentials.
+
+# User
+user_name = 'SocMediaClass'
+
+# Registered application (script-type)
+app_id = '9x2ZyeZZuAGxNbZXKNKIzw'
+app_secret = 'q6kEF83HonNoixaya-TfTJTtQU4xMw'
+
